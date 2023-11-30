e01b018e28740e9c38558be7713d4b1710003d1a
Robby Widyahartono <robby.widyahartono@komunal.co.id>
2023-11-30 22:02:47 +0700

    integrate DI

--- src/common/auth/auth.guard.spec.ts
+++ src/common/auth/auth.guard.spec.ts
@@ -1,25 +1,23 @@
 import { ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from './auth.guard';
 import { createMock } from '@golevelup/ts-jest';
+import { LoggerService } from '../../logger.service';
 
 describe('AuthGuard', () => {
-  const authGuard = new AuthGuard();
-  it('should be defined', () => {
-    expect(authGuard).toBeDefined();
-  });
+  const authGuard = new AuthGuard(new LoggerService());
 
-  it(`should return true with valid api key header`, () => {
+  it(`should return false with invalid apiKey`, () => {
     const context = createMock<ExecutionContext>({
       switchToHttp: () => ({
         getRequest: () => ({
           headers: {
-            'x-api-key': `SECRET`,
+            'x-api-key': `INVALID`,
           },
         }),
       }),
     });
     const result = authGuard.canActivate(context);
-    expect(result).toBe(true);
+    expect(result).toBe(false);
   });
 
   it(`should return false when api key does not exist`, () => {
